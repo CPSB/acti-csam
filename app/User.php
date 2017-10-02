2b59ab6d3d3a174e55fe5a26f01891e16f0c6ec8
Tjoosten <Tjoosten3@gmail.com>
2017-10-02 20:43:21 +0200

    Add property docblock on the user model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,17 @@ namespace ActivismeBE;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Database Eloquent Model 'User'
+ *
+ * @property integer            $id                 The primary key from the database row.
+ * @property string             $name               The username column.
+ * @property string             $email              The user his email address.
+ * @property string             $password           The password column from the user.
+ * @property string             $remember_token     The remember_token from the user in the database.
+ * @property \Carbon\Carbon     $created_at         The timestamp when the user is created.
+ * @property \Carbon\Carbon     $updated_at         The timestamp when the user is last updated.
+ */
 class User extends Authenticatable
 {
     use Notifiable;
@@ -14,16 +25,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 }

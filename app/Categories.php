ce2c5b42e695edb8aae19fd201d0dfea5648b9e4
Tjoosten <Tjoosten3@gmail.com>
2017-10-02 18:04:55 +0200

    Complete categories database table

--- app/Categories.php
+++ app/Categories.php
@@ -4,7 +4,38 @@ namespace ActivismeBE;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Eloquent database model 'Categories'
+ *
+ * @property integer        $id             The primary key from the category in the db.
+ * @property integer        $author_id      The id form the user who has created the category.
+ * @property string         $color          The HEX value from the category.
+ * @property string         $name           The name form the category.
+ * @property string         $description    The description from the category.
+ * @property \Carbon\Carbon $created_at     The timestamp when the category has been created.
+ * @property \Carbon\Carbon $updated_at     The timestamp when the category last has been updated.
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\ActivismeBE\User[] $author
+ */
 class Categories extends Model
 {
-    //
+    // TODO: Build up the migration.
+    // TODO: Build up the testing factory.
+
+    /**
+     * Mss-assign fields for the database table.
+     *
+     * @var array
+     */
+    protected $fillable = ['author_id', 'color', 'name', 'description'];
+
+    /**
+     * Data relation for the author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }

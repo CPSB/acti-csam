531f87292652e00db391d16f7212837217120eec
Tjoosten <Tjoosten3@gmail.com>
2017-10-03 21:29:05 +0200

    Implement commet model

--- app/Comments.php
+++ app/Comments.php
@@ -4,7 +4,33 @@ namespace ActivismeBE;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Eloquent databasemodel 'Comments'
+ *
+ * @property integer        $id         The unique identifier in the storage.
+ * @property integer        $author_id  The user id form the author.
+ * @property string         $message    The comment message.
+ * @property \Carbon\Carbon $created_at The timestamp when the record has been created
+ * @property \Carbon\Carbon $updated_at The timestamp when the record last has been updated.
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\ActivismeBE\User[] $author.
+ */
 class Comments extends Model
 {
-    //
+    /**
+     * Mass-assign fields for the mass-assign fields.
+     *
+     * @var array
+     */
+    protected $fillable = ['author_id', 'message'];
+
+    /**
+     * Data relation for the comment author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }

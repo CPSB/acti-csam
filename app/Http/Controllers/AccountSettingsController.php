6d58d46c41eb9ce9610f27663bff018788274bd8
Tjoosten <Tjoosten3@gmail.com>
2017-10-05 17:27:33 +0200

    Update

--- app/Http/Controllers/AccountSettingsController.php
+++ app/Http/Controllers/AccountSettingsController.php
@@ -2,6 +2,7 @@
 
 namespace ActivismeBE\Http\Controllers;
 
+use Gate;
 use ActivismeBE\Http\Requests\{AccountInfoValidator, AccountSecurityValidator};
 use ActivismeBE\Repositories\UsersRepository;
 use Illuminate\Http\{Response, RedirectResponse};
@@ -17,18 +18,17 @@ use Intervention\Image\Facades\Image;
  */
 class AccountSettingsController extends Controller
 {
-    // TODO: Write tests for the AccountSettingsController
-
     private $user;            /** @var Auth            $user            */
     private $usersRepository; /** @var UsersRepository $usersRepository */
 
     /**
      * AccountSettingsController constructor.
      *
+     * @param  Auth            $user            The authentication user instance.
      * @param  UsersRepository $usersRepository The users abstraction layer between database and controller.
      * @return void
      */
-    public function __construct(UsersRepository $usersRepository)
+    public function __construct(UsersRepository $usersRepository, $user)
     {
         $this->middleware('auth');
         // $this->middleware('role:admin')              // TODO: Build up and register the middleware.
@@ -91,6 +91,30 @@ class AccountSettingsController extends Controller
 
         if ($this->usersRepository->update($input->except(['_token', 'userImage']), auth()->user()->id)) {
             flash("Jouw account informatie is aangepast.")->success();
+        }
+
+        return redirect()->back(Response::HTTP_FOUND);
+    }
+
+    /**
+     * Delete an user account out off the system. 
+     * 
+     * @param  integer $accountId The primary key in the storage for the account.
+     * @return \Illuminate\Http\RedirectResponse 
+     */
+    public function delete($accountId): RedirectResponse
+    {
+        $user = $this->usersRepository->delete($accountId) ?: abort(403);
+
+        // TODO: Register delete policy on the users model.
+        if (Gate::allows('delete', $user)) {                // Authenticated user is allowed to delete the account.
+            if ($user->delete()) {                          // The user has been deleted.
+                if ($user->id == auth()->user()->id) {      // The given user is the currently authencated user.
+                    auth()->logout();
+                }
+
+                flash("Wij hebben {$user->name} verwijderd uit het systeem.");
+            }
         }
 
         return redirect()->back(Response::HTTP_FOUND);
